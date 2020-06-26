ea75f2cff9cfcaaf86e5c5d83c84a950edc681fd
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-06-26 12:00:12 +0530

    improved menu

--- packages/layout/src/components/side-bar.tsx
+++ packages/layout/src/components/side-bar.tsx
@@ -17,42 +17,42 @@ const SideBar: React.FC<any> = () => {
       <nav className="text-white text-base font-semibold pt-3">
         <NavLink
           to="/dashboard"
-          className="menu-item flex items-center text-white py-4 mb-1 last:mb-0 pl-6 active:text-new-gray-darker active:bg-ocean-light hover:bg-ocean-light hover:text-new-gray-darker"
+          className="menu-item flex items-center text-white py-4 pl-6 active:text-new-gray-darker active:bg-ocean-light hover:bg-ocean-light hover:text-new-gray-darker hover:opacity-75"
         >
           <i className="fas fa-tachometer-alt mr-3"></i>
           Dashboard
         </NavLink>
         <NavLink
           to="/blank"
-          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker mb-1 last:mb-0"
+          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker hover:opacity-75"
         >
           <i className="fas fa-sticky-note mr-3"></i>
           Blank Page
         </NavLink>
         <NavLink
           to="/tables"
-          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker mb-1 last:mb-0"
+          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker hover:opacity-75"
         >
           <i className="fas fa-table mr-3"></i>
           Tables
         </NavLink>
         <NavLink
           to="/forms"
-          className="flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker mb-1 last:mb-0"
+          className="flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker hover:opacity-75"
         >
           <i className="fas fa-align-left mr-3"></i>
           Forms
         </NavLink>
         <NavLink
           to="/tabs"
-          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker mb-1 last:mb-0"
+          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker hover:opacity-75"
         >
           <i className="fas fa-tablet-alt mr-3"></i>
           Tabbed Content
         </NavLink>
         <NavLink
           to="/calendar"
-          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:text-new-gray-darker mb-1 last:mb-0"
+          className="menu-item flex items-center active:bg-ocean-light active:text-new-gray-darker text-white py-4 pl-6 hover:bg-ocean-light hover:opacity-75 hover:text-new-gray-darker"
         >
           <i className="fas fa-calendar mr-3"></i>
           Calendar
